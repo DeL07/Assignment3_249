60f2151a5642cd1b61cb3d7ecc24f81012227a49
DeL07 <lscarle07@gmail.com>
2016-03-29 16:13:24 -0400

    Update 1
    Added comments and instructions in the comments

--- src/Order.java
+++ src/Order.java
@@ -2,8 +2,9 @@
 	all the common members a bid and an offer would have. Then create a BidOrder
 	object and an OfferOrder object, both which extend the parent offer object
 	(depending on your design, they may not add any new instance variables but will allow
-	you to tell the types of orders apart using instance of and/or getClass()). An
-	order of either type should have a string ID for the person submitting the order, a
+	you to tell the types of orders apart using instance of and/or getClass()). 
+	
+	An order of either type should have a string ID for the person submitting the order, a
 	double price, and an int volume. They should have appropriate getters and setters
 	for the variables, a toString method (which we will return to below when discussing
 	the anonymous interface), and other standard methods. In this assignment, we will
@@ -44,4 +45,39 @@ public class Order {
 	public String toString() {
 		return "OrderBook [ID=" + ID + ", price=" + price + ", volume=" + volume + "]";
 	}
+
+	// Matching engine - instructions collapsed
+	/* @formatter:off 
+	 * 
+	The matching engine should take a BidOrder or OfferOrder object, attempt to
+	match it to the orders in the book and report on any resulting trades.
+	Consider a OfferOrder (the BidOrder case is similar). The matching algorithm will
+	go through the following steps:
+		I. Is the offer price lower than the BestBid? Yes.
+			A. Match the order to the best bid, the second best bid, etc. until:
+				1. The full volume of the OfferOrder has been satisfied. The last bid to match to
+					the offer may still exist in the book but with a lower volume.
+				2. No bids are left in the book. In this case, leave the remaining volume of the
+					order as a new (best) offer.
+				3. All the bids that are left in the book have a lower price than the offer. In this
+					case, leave the remaining volume of the order as a new (best) offer.
+			B. For each matched order, report the trade (including identities of the buyers and
+				seller).
+			C. Update the BestBid
+		II. Is the offer price lower than the BestBid? No, or there are no bids in the book yet.
+			A. Is the offer price lower than the BestOffer? Yes.
+				1. Set the offer as the new BestOffer.
+			B. Is the offer price lower than the BestOffer? No it is equal.
+				1. For two (or more) offers at the same price, the order that comes first gets first
+					priority. This means placing new orders closer to the outside of the book
+					(further from the best bid/offer).
+			C. Is the offer price lower than the BestOffer? No it is greater.
+				1. Find the right place in the book for insertion.
+			D. Is the offer price lower than the BestBid? No, there are no bids in the book yet. 
+				1. Set the offer as the new BestOffer. @formatter:on */
+
+	public static void main(String[] args) {
+
+	}
+
 }
--- src/OrderBook.java
+++ src/OrderBook.java
@@ -1,4 +1,27 @@
+/* @formatter:off
+ * 	Create an OrderBook object as a linked data structure. Specifically, it
+	should be a doubly linked list where unmatched Order objects can be inserted, and
+	the list is always sorted from largest price to smallest price. The nodes of the list should
+	be implemented as an inner class within the order book object. Because of the way
+	orders are matched, the resulting list will always have all the offers at the start of the list
+	and then all the bids being at the end of the list.
+	
+	While every order in the book is important, two orders of particular importance. They
+	are called the best bid and best offer. The best bid is the bid with the largest price and
+	the best offer is the offer with the lowest price. These two orders will be beside each
+	other, somewhere in the middle of the order book. The difference in price between the
+	best bid and best order is called the spread. For the doubly linked list in the
+	OrderBook object, always maintain a pointer to the BestBid node and the
+	BestOffer node. This data structure will allow you to jump directly to either the best
+	bid or the best offer and walk through the book in either direction.
+	
+	Additionally, your OrderBook object should implement the following methods:
+		• public void outputBook(): print out the OrderBook (note interface requirements below)
+		• public void outputBBO(): print out the best bid and offer (BBO) @formatter:on*/
 
 public class OrderBook {
-
+	
+	
+	
+	
 }

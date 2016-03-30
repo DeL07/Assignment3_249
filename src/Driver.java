46a848b2d5a61632948748a7b6aadddce711b159
DeL07 <lscarle07@gmail.com>
2016-03-30 16:44:04 -0400

    TEST

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,36 @@
 import java.util.TreeSet;
 
+//Matching engine - instructions collapsed
+	/* @formatter:off 
+	 * 
+	The matching engine should take a BidOrder or OfferOrder object, attempt to
+	match it to the orders in the book and report on any resulting trades.
+	Consider a OfferOrder (the BidOrder case is similar). The matching algorithm will
+	go through the following steps:
+		I. Is the offer price lower than the BestBid? Yes.
+			A. Match the order to the best bid, the second best bid, etc. until:
+				1. The full volume of the OfferOrder has been satisfied. The last bid to match to
+					the offer may still exist in the book but with a lower volume.
+				2. No bids are left in the book. In this case, leave the remaining volume of the
+					order as a new (best) offer.
+				3. All the bids that are left in the book have a lower price than the offer. In this
+					case, leave the remaining volume of the order as a new (best) offer.
+			B. For each matched order, report the trade (including identities of the buyers and
+				seller).
+			C. Update the BestBid
+		II. Is the offer price lower than the BestBid? No, or there are no bids in the book yet.
+			A. Is the offer price lower than the BestOffer? Yes.
+				1. Set the offer as the new BestOffer.
+			B. Is the offer price lower than the BestOffer? No it is equal.
+				1. For two (or more) offers at the same price, the order that comes first gets first
+					priority. This means placing new orders closer to the outside of the book
+					(further from the best bid/offer).
+			C. Is the offer price lower than the BestOffer? No it is greater.
+				1. Find the right place in the book for insertion.
+			D. Is the offer price lower than the BestBid? No, there are no bids in the book yet. 
+				1. Set the offer as the new BestOffer. @formatter:on */
+
+
 public class Driver {
 
 	public static void main(String[] args) {
@@ -13,15 +44,6 @@ public class Driver {
 		BidOrder b2 = new BidOrder("Lana", 147.0, 200);
 		BidOrder b3 = new BidOrder("Jaba", 146.6, 100);
 		BidOrder b4 = new BidOrder("Fana", 146.5, 50);
-		
-//		OrderBook.offerBook.add(o1);
-//		OrderBook.offerBook.add(o2);
-//		OrderBook.offerBook.add(o3);
-//		
-//		OrderBook.bidBook.add(b1);
-//		OrderBook.bidBook.add(b2);
-//		OrderBook.bidBook.add(b3);
-//		OrderBook.bidBook.add(b4);
 		
 		OrderBook.addOffer(o1);
 		OrderBook.addOffer(o2);
--- src/Order.java
+++ src/Order.java
@@ -55,39 +55,7 @@ public class Order implements Comparable, IAnonymous {
 		return "OrderBook [ID=" + ID + ", price=" + price + ", volume=" + volume + "]";
 	}
 
-	// Matching engine - instructions collapsed
-	/* @formatter:off 
-	 * 
-	The matching engine should take a BidOrder or OfferOrder object, attempt to
-	match it to the orders in the book and report on any resulting trades.
-	Consider a OfferOrder (the BidOrder case is similar). The matching algorithm will
-	go through the following steps:
-		I. Is the offer price lower than the BestBid? Yes.
-			A. Match the order to the best bid, the second best bid, etc. until:
-				1. The full volume of the OfferOrder has been satisfied. The last bid to match to
-					the offer may still exist in the book but with a lower volume.
-				2. No bids are left in the book. In this case, leave the remaining volume of the
-					order as a new (best) offer.
-				3. All the bids that are left in the book have a lower price than the offer. In this
-					case, leave the remaining volume of the order as a new (best) offer.
-			B. For each matched order, report the trade (including identities of the buyers and
-				seller).
-			C. Update the BestBid
-		II. Is the offer price lower than the BestBid? No, or there are no bids in the book yet.
-			A. Is the offer price lower than the BestOffer? Yes.
-				1. Set the offer as the new BestOffer.
-			B. Is the offer price lower than the BestOffer? No it is equal.
-				1. For two (or more) offers at the same price, the order that comes first gets first
-					priority. This means placing new orders closer to the outside of the book
-					(further from the best bid/offer).
-			C. Is the offer price lower than the BestOffer? No it is greater.
-				1. Find the right place in the book for insertion.
-			D. Is the offer price lower than the BestBid? No, there are no bids in the book yet. 
-				1. Set the offer as the new BestOffer. @formatter:on */
-
-	public static void main(String[] args) {
-
-	}
+	
 
 
 //	@Override
@@ -99,9 +67,9 @@ public class Order implements Comparable, IAnonymous {
 //	}
 
 	@Override
-	public int compareTo(Object o) {
+	public double compareTo(Object o) {
 		Order order1 = (Order) o;
-		return (int) (order1.getPrice() - this.price);
+		return (double) (order1.getPrice() - this.price);
 	}
 
 	@Override
